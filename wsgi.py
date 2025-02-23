d9fa43e9f557ea3286b1537926184ab2753d8c3a
damir7182 <cat792111@gmail.com>
2025-02-23 14:42:06 +0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,12 @@
 from app import app, init_db
 
-# Инициализация базы данных при запуске
-with app.app_context():
-    init_db()
+try:
+    # Инициализация базы данных при запуске
+    with app.app_context():
+        init_db()
+    print("База данных успешно инициализирована.")
+except Exception as e:
+    print(f"Ошибка при инициализации базы данных: {e}")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)  # Включаем debug-режим для удобства разработки
